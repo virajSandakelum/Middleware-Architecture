bd6fc7dfd7e7bedb03c970151c7c909d35933e74
Binula Rasanjith <binula28@gmail.com>
2023-07-16 12:52:07 +0530

    thoroughly commented the source code

--- Task_1_The_Client_Server_Application/Client.java
+++ Task_1_The_Client_Server_Application/Client.java
@@ -4,23 +4,31 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Client {
-    private Socket socket;
-    private PrintWriter out;
-    private BufferedReader in;
-    private BufferedReader stdIn;
+    private Socket socket; // socket object to connect to the server
+    private PrintWriter out; // output stream to send data to the server
+    private BufferedReader in; // input stream to receive data from the server
+    private BufferedReader stdIn; // input stream to read data from the console
 
+    /* Method to start the client */
     public void start(String host, int port) {
         try {
+            // create a new socket object to connect to the server
             socket = new Socket(host, port);
             System.out.println("Client: connected to " + host + ":" + port);
 
+            // create a new output stream to send data to the server
             out = new PrintWriter(socket.getOutputStream(), true);
+            // create a new input stream to receive data from the server
             // in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            // create a new input stream to read data from the console
             stdIn = new BufferedReader(new InputStreamReader(System.in));
 
             String inputLine;
+            // read data from the console and send it to the server
             while ((inputLine = stdIn.readLine()) != null) {
                 out.println(inputLine);
+                // if the input is "terminate", break the loop
                 if (inputLine.equals("terminate")) {
                     System.out.println("Client: terminating");
                     break;
@@ -35,14 +43,19 @@ public class Client {
         }
     }
 
+    /* Method to stop the client */
     public void stop() {
         try {
+            // close the input stream to receive data from the server
             if (in != null)
                 in.close();
+            // close the output stream to send data to the server
             if (out != null)
                 out.close();
+            // close the input stream to read data from the console
             if (stdIn != null)
                 stdIn.close();
+            // close the socket object
             if (socket != null)
                 socket.close();
         } catch (Exception e) {
@@ -51,14 +64,17 @@ public class Client {
     }
 
     public static void main(String[] args) {
+        // check if the command line arguments are valid
         if (args.length != 2) {
             System.err.println("Usage: java Client <host name> <port number>");
             System.exit(1);
         }
 
+        // get the hostname and port number from the command line arguments
         String host = args[0];
         int port = Integer.parseInt(args[1]);
 
+        // create a new client object and start the client
         Client client = new Client();
         client.start(host, port);
     }
--- Task_1_The_Client_Server_Application/Server.java
+++ Task_1_The_Client_Server_Application/Server.java
@@ -6,22 +6,27 @@ import java.net.Socket;
 import java.net.ServerSocket;
 
 public class Server {
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
+    private ServerSocket serverSocket; // ServerSocket object for the server's socket
+    private Socket clientSocket; // Socket object for the client's socket
+    private PrintWriter out; // PrintWriter object for the output stream to the client
+    private BufferedReader in; // BufferedReader object for the input stream from the client
 
+    /* Start the server on the given port */
     public void start(int port) {
         try {
+            // Create a new server socket on the given port
             serverSocket = new ServerSocket(port);
             System.out.println("Server: listening on port " + port);
 
+            // Wait for a client to connect
             clientSocket = serverSocket.accept();
             System.out.println("Client connected: " + clientSocket.getInetAddress());
 
+            // Create input and output streams for the client socket
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             // out = new PrintWriter(clientSocket.getOutputStream(), true);
 
+            // Read input from the client until "terminate" is received
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
                 if (inputLine.equalsIgnoreCase("terminate")) {
@@ -31,6 +36,7 @@ public class Server {
                 System.out.println("Client: " + inputLine);
             }
 
+            // Close the connection with the client
             System.out.println("Client: disconnected");
         } catch (IOException e) {
             e.printStackTrace();
@@ -39,6 +45,7 @@ public class Server {
         }
     }
 
+    /* Stop the server and close all connections */
     public void stop() {
         try {
             if (in != null)
@@ -55,12 +62,16 @@ public class Server {
     }
 
     public static void main(String[] args) {
+        // check if the command line arguments are valid
         if (args.length != 1) {
             System.err.println("Usage: java Server <port number>");
             System.exit(1);
         }
 
+        // get the port number from the command line arguments
         int port = Integer.parseInt(args[0]);
+
+        // create a new server object and start the server
         Server server = new Server();
         server.start(port);
     }
