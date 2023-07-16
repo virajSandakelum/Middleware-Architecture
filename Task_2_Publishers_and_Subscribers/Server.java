77af88f0f8a664e6d01087899fef8029cc8c351b
virajSandakelum <2020cs011@stu.ucsc.cmb.ac.lk>
2023-07-16 13:35:15 +0530

    make runnable interface

--- Task_2_Publishers_and_Subscribers/Server.java
+++ Task_2_Publishers_and_Subscribers/Server.java
@@ -1,3 +1,71 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
-    
+    private int clientCount = 0;
+    private ServerSocket serverSocket;
+    private List<ClientHandler> clients = new ArrayList<>();
+
+    public void start(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server started on port " + port);
+        } catch (Exception error) {
+            error.printStackTrace();
+            System.out.println("Server failed to start on port " + port);
+        }
+
+        while(true){
+            try{
+                Socket clientSocket = serverSocket.accept();
+                ClientHandler client = new ClientHandler(clientSocket,clientCount);
+                clients.add(client);
+
+                Thread thread = new Thread(client);
+                thread.start();
+            }catch(Exception error){
+                error.printStackTrace();
+                System.out.println("Client failed to connect");
+            }
+        }
+    }
+
+    private class ClientHandler implements Runnable{
+        private int id;
+        private Socket clientSocket;
+        private PrintWriter out;
+        private BufferedReader in;
+        private boolean isPublisher = false;
+
+        public ClientHandler(Socket clientSocket, int id){
+            this.clientSocket = clientSocket;
+            this.id = id;
+            System.out.println("Client"+ id+" connected");
+        }
+
+        public void run(){
+            try{
+                out = new PrintWriter(clientSocket.getOutputStream(),true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                String inputLine = in.readLine();
+                System.out.println("Client"+id+" is a "+inputLine.toLowerCase());
+                isPublisher = inputLine.equalsIgnoreCase("PUBLISHER");
+
+                while((inputLine = in.readLine()) != null){
+                    String message = "Client"+id+" sent: "+ inputLine;
+                    System.out.println(message);
+
+                    
+                }
+            }
+        }
+    }
 }
+
+
