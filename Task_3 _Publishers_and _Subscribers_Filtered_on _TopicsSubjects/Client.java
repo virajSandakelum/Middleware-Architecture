03559eecc1728b8d88af2af9a1e9cd0057fb4398
Thimet <92925044+Thimet@users.noreply.github.com>
2023-07-16 14:43:40 +0530

    Completed remaining part of the Client.java

--- Task_3 _Publishers_and _Subscribers_Filtered_on _TopicsSubjects/Client.java
+++ Task_3 _Publishers_and _Subscribers_Filtered_on _TopicsSubjects/Client.java
@@ -1,11 +1,11 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.io.BufferedReader; //BufferedReader reads text from a character-input stream, buffering characters so as to provide for the efficient reading of characters, arrays, and lines.
+import java.io.InputStreamReader; //An InputStreamReader is a bridge from byte streams to character streams: It reads bytes and decodes them into characters using a specified charset.
+import java.io.PrintWriter; //PrintWriter prints formatted representations of objects to a text-output stream.
+import java.net.Socket; //This class implements client sockets (also called just "sockets"). A socket is an endpoint for communication between two machines.
 
 public class Client {
 
-    private Socket clientSocket;
+    private Socket clientSocket; 
     private PrintWriter out;
     private BufferedReader in;
     private BufferedReader stdIn;
@@ -14,14 +14,14 @@ public class Client {
 
     public void start(String host, int port) {
         try {
-            clientSocket = new Socket(host, port);
-            System.out.println("Client connected to " + host + ":" + port);
+            clientSocket = new Socket(host, port); //Creates a stream socket and connects it to the specified port number on the named host.
+            System.out.println("Client connected to " + host + ":" + port); //Prints the message on the console
 
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            stdIn = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(clientSocket.getOutputStream(), true); //Creates a new PrintWriter, without automatic line flushing, from an existing OutputStream.
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); //Creates a buffering character-input stream that uses a default-sized input buffer.
+            stdIn = new BufferedReader(new InputStreamReader(System.in)); //Creates a buffering character-input stream that uses a default-sized input buffer.
 
-            String inputLine;
+            String inputLine; 
             if (isPublisher) {
                 out.println("publisher\n" + topic);
                 while ((inputLine = stdIn.readLine()) != null) {
