445d38719d1b7b22a12eda665adc6a920be3f75e
Bhanuka Rajakaruna <87462362+BhanukaRajk@users.noreply.github.com>
2023-07-16 13:11:51 +0530

    Terminating function define

--- Task_2_Publishers_and_Subscribers/Client.java
+++ Task_2_Publishers_and_Subscribers/Client.java
@@ -4,7 +4,7 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
-    
+
     private Socket clientSocket;
     private PrintWriter out;
 
@@ -13,22 +13,52 @@ public class Client {
 
     private static boolean isPublisher = false;
 
-    public void start(){
-        clientSocket = new Socket(host, port);
-        System.out.println("Client connected to "+host+":"+port);
 
-        out = new PrintWrite(clientSocket.getOutputStream(), autoFlush:true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        stdIn = new BufferedReader(new InputStreamReader(System.in));
+    public void start(String host, int port) {
+        try {
+            clientSocket = new Socket(host, port);
+            System.out.println("Client connected to " + host + ":" + port);
 
-        if(isPublisher){
-            out.println("Publisher");
-            String inputLine;
-            while ((inputLine = stdIn.readLine()) != null) {
-                out.println(inputLine);
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            if(isPublisher){
+                out.println("Publisher");
+                String inputLine;
+                while ((inputLine = stdIn.readLine()) != null) {
+                    out.println(inputLine);
+                }
+            } else {
+                out.println("subcriber");
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    System.out.println(inputLine);
+                }
             }
+        } catch (Exception error) {
+            error.printStackTrace();
+            System.out.println("Client Failed to connect");
+        } finally {
+            terminateConnection()
         }
-
     }
+
+    public void terminateConnection() {
+        try {
+            if (clientSocket != null)
+                clientSocket.close();
+            if (out != null)
+                out.close();
+            if (in != null)
+                in.close();
+            if (stdIn != null)
+                stdIn.close();
+        } catch (Exception error) {
+            error.printStackTrace();
+        }
+    }
+
+
 
 }
